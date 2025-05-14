4936a4b18d9836763d71af8025d65b04e1e5c09d
HokulaniTopping <hokulanigit@gmail.com>
2025-05-14 12:50:49 -0700

    made a utils file for prettier code or tokenizing

--- utils.py
+++ utils.py
@@ -1,11 +1,12 @@
 import re
 from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
+# from nltk.tokenize import word_tokenize
+import re
 
 stemmer = PorterStemmer()
 
 def tokenize_and_stem(text):
     # Clean and tokenize
-    tokens = word_tokenize(text)
+    tokens = re.findall(r'\b[a-zA-Z0-9]+\b', text.lower())
     cleaned = [re.sub(r'\W+', '', token.lower()) for token in tokens if token.isalnum()]
     return [stemmer.stem(token) for token in cleaned if token]
